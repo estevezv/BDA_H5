efd3946b585da412b6057b57e05d013bf1c4f54e
estevezv <50021709+estevezv@users.noreply.github.com>
2019-04-30 13:46:34 +0300

    Update keras-dvc-cnn-simple.py

--- keras-dvc-cnn-simple.py
+++ keras-dvc-cnn-simple.py
@@ -166,7 +166,6 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 model.add(Dense(64, activation='relu'))
-model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
 
